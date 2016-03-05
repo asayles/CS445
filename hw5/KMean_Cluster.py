cd76ae7122038a51ac849f742c88666046fa0c61
AJ Sayles <armondo.j.sayles@intel.com>
2016-03-05 13:33:00 -0800

    hw5

---
+++ hw5/KMean_Cluster.py
@@ -0,0 +1,37 @@
+#!/c/Python27/python
+
+import csv
+
+def createCentroids(size_of_centroids, num_of_centroids):
+    #create size number of centroids "m"
+    centroids = []
+    for i in num_of_centroids:
+        for i in sizeof_centroids:
+            
+    
+    
+    return M_Values
+
+def loadFile(file_name):
+    with open(file_name,'a+') as raw_file:
+        reader = csv.reader(raw_file)
+        raw_list = list(reader)
+    return raw_list 
+    
+def euclidHelper(a,b):
+    print "helper"    
+
+def euclidianDistance(a, b):
+    # /nfs/pdx/home/ajsayles/git/ec-pdx-app-force/updateDB.py
+    print "euclid"
+    sum = 0
+    for index in a:
+        for index b:
+    
+    euclidHelper(a,b)
+    
+    
+
+# expr1 = k =10   
+test_data = loadFile('optdigits/optdigits.test')
+print test_data
