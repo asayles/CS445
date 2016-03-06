e464642b069fb0bbf26dd2c65dcdd0e47a6ed181
asayles <asayles@pdx.edu>
2016-03-06 15:37:31 -0800

    finds sse

--- hw5/KMean_Cluster.py
+++ hw5/KMean_Cluster.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-import csv
+import csv, math, sys
+from pprint import pprint as pp
 from random import randint
 
-RANGE = 16
+
 
 def createCentroids(size_of_centroids, num_of_centroids):
     #create size number of centroids "m"
@@ -11,7 +12,7 @@ def createCentroids(size_of_centroids, num_of_centroids):
     for i in range(num_of_centroids):
         foo = []
         for j in range(size_of_centroids):
-            foo.append(randint(0,RANGE))
+            foo.append(randint(0,CENTROID_RANGE))
         centroids.append(foo)    
     return centroids
 
@@ -21,81 +22,116 @@ def loadFile(file_name):
         raw_list = list(reader)
     return raw_list 
     
-def euclidHelper(a,b):
-    print "helper"    
 
 def euclidianDistance(centroids, instance):
     # return list of distances to each centroid
-    print "===euclidD==="
-    print len(centroids[0]), len(instance)
-    sums = []
+    distances = []
     for centroid in centroids:
         sum = 0
-        for index in range(len(instance) - 1):
-            sum += centroid[index] - int(instance[index])
-        sums.append(sum)
-    return sums
+        # find the distance to each centroid for this instance
+        for index in range((len(instance) - 1)):
+            sum += math.pow(centroid[index] - int(instance[index]), 2)
+        distances.append(sum)
+    return distances
 
 
 def findClosestCentroid(distances):
     # return index of highest number 
     # first wins, no tie breaking
-    print "===findClosestCentroid==="
     winner = 0
-    print "len distances: ", len(distances)
-    for i in range(len(distances) - 1):
-        print i
-        if distances[i] > distances[winner]:
+    for i in range(len(distances)):
+        if distances[i] < distances[winner]:
             winner = i
     return winner
 
  
 def findYourCenter(train_data, centroids):
     # create a list of instances that belong to a centroid
-    print "===findYourCenter==="
-    print "len centroids", len(centroids)
-    centroid_groups = [[]] * len(centroids)
+    centroid_groups = []
+    for i in range(len(centroids)):
+        centroid_groups.append([])
     counter = 0
-    print "num of centroid groups: ",len(centroid_groups)
     for instance in train_data:
+        # get the distance from each centroid
         distances = euclidianDistance(centroids, instance)
-        print "distances: ",distances
-        myCentroid = findClosestCentroid(distances)
-        centroid_groups[myCentroid].append(counter)
+
+        # find the centroid that's closest
+        my_centroid = findClosestCentroid(distances)
+
+        # add the instances index to the list of it's closest centroid
+        centroid_groups[my_centroid].append(counter)
         counter += 1
     return centroid_groups
 
-def adjustCentroids(centroid_groups, train_data):
-    # return list of new centroids
-    print "===adjustCentroids==="
-    num_groups = len(centroid_groups)
-    centroids = []
-    for i in range(num_groups):
-        centroids.append(adjustCentroidsHelper(centroid_groups[i], train_data))
-    return centroids
 
-def adjustCentroidsHelper(centroid_group, train_data):
-    # return mean for all the features in the centroid group
-    print "adjustCentroidsHelper"
-    centroid_sum = [0] * len(train_data[0])
-    
+def adjustOneCentroid(centroid_group, train_data):
+    # sum all the features for this centroid group, then divide by size
+    centroid_sum = [0] * (len(train_data[0]) - 1)
     for i in centroid_group:
-        print centroid_sum
-        print train_data[i]
         instance = []
-        for j in range(len(train_data[i])):
+        for j in range(len(train_data[i]) - 1):
             instance.append(int(train_data[i][j]))
-        print instance
         centroid_sum = [x + y for x, y in zip(centroid_sum, instance)]
     
     for k in range(len(centroid_sum)):
-        instance[k] = int(instance[k]) / len(centroid_group)
+        centroid_sum[k] = int(centroid_sum[k]) / len(centroid_group)
     
     return centroid_sum
 
 
-# expr1 = k =10   b
+def adjustCentroids(centroid_groups, train_data):
+    # return list of new centroids
+    num_groups = len(centroid_groups)
+    centroids = []
+    for i in range(num_groups):
+        centroids.append(adjustOneCentroid(centroid_groups[i], train_data))
+    return centroids
+
+
+def findSSE(centroids,centroid_groups, instances):
+    sum = 0
+    for i in range(len(centroids)):
+        for j in centroid_groups[i]:
+            for k in range(len(centroids[i])):
+                sum += math.pow(centroids[i][k] - int(instances[j][k]),2)
+    return sum
+
+
+#==============
+# EXPR 1
+#=============
+K = 10
+num_features = 64
+CENTROID_RANGE = 16
+
 train_data = loadFile('optdigits/optdigits.train')
-centroids = createCentroids(64,10)
-centroid_groups = findYourCenter(train_data, centroids)
-centroids = adjustCentroids(centroid_groups, train_data)
+# print"data: ", train_data
+
+new_centroids = []
+centroid_groups = []
+
+centroids = createCentroids(num_features,K)
+# centroids = [[1,0],[4,4]]
+# print "centroids: ",centroids
+
+while centroids != new_centroids:
+    if new_centroids:
+        centroids = new_centroids
+    empty_group_found = True
+    while empty_group_found:
+        empty_group_found = False
+        centroid_groups = findYourCenter(train_data, centroids)
+        for i in centroid_groups:
+            if len(i) == 0:
+                print "found a weasel"
+                empty_group_found = True
+                centroids = createCentroids(num_features,K)
+                
+
+    new_centroids = adjustCentroids(centroid_groups, train_data)
+    # print "centroids: ", centroids
+    # print "nCentroids: ", new_centroids
+
+sse = findSSE(new_centroids, centroid_groups, train_data)
+print "sse: ", sse
+print "\n"
